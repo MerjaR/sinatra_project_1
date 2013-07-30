e6db6df2730b3779d78c59f216568a5cd0c0335b
Merja Rossi <merja.rossi@gmail.com>
2013-07-30 18:30:07 +0100

    Added Gemfile.lock

--- app.rb
+++ app.rb
@@ -3,3 +3,37 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+get '/love' do
+    erb :love
+end
+
+
+get '/:name' do
+
+    "Hello #{params[:name].capitalize}!"
+end
+
+get '/:name1/:name2/love' do
+
+	@name1 = params[:name1].capitalize!
+	@name2 = params[:name2].capitalize!
+    erb :love
+
+end
+
+get '/:name1/:name2/hearts' do
+
+	@name1 = params[:name1].capitalize!
+	@name2 = params[:name2].capitalize!
+    erb :hearts
+
+end
+
+get '/:name/duck' do
+
+	@name = params[:name].capitalize!
+    erb :duck
+
+end
+
